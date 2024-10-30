7a0fac1cb50f1d42948e8bc100e584ff3d94bb19
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2024-10-31 04:45:07 +0530

    Created login and register UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./Routes/index.jsx";
 
 function App() {
-
   return (
-    <div>
-      hello
-    </div>
-  )
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Constants/routes.jsx
@@ -0,0 +1,7 @@
+const ROUTES = {
+  HOME : '/home',
+  LOGIN: "/login",
+  SIGNUP: "/signup",
+};
+
+export { ROUTES };
