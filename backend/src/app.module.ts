5184adf8611b5d10c7bdcbdc48a35b58037adb53
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2024-11-10 17:26:01 +0530

    Render all scheduled streams

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,30 +3,31 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from './users/Entity/user.entity';
 import { UsersModule } from './users/users.module';
-import { ScheduleMeetModule } from './meeting/meeting.module';
-import { ScheduleMeet } from './meeting/Entity/meeting.entity';
+import { ScheduleStreamModule } from './scheduleStream/stream.module';
+import { ScheduleStream } from './scheduleStream/Entity/stream.entity';
 import * as dotenv from 'dotenv'
 import * as path from 'path';
-
+ScheduleStreamModule
 dotenv.config({ path: path.join(__dirname, '../.env') })
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    entities: [Users, ScheduleMeet],
-    password: process.env.DB_Password,
-    synchronize: true,
-    logging: true,
-  }),
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      entities: [Users, ScheduleStream],
+      password: process.env.DB_Password,
+      synchronize: true,
+      logging: true,
+    }),
     AuthModule,
     UsersModule,
-    ScheduleMeetModule,
+    ScheduleStreamModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
