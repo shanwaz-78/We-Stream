760611d59eb9d9a8c6ab88719d6f770bc8229673
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2024-11-13 00:42:55 +0530

    logout user on token expiry

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -5,19 +5,23 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { PassportJWTStrategy } from './Strategy/PassportJWT.Strategy';
 import { UsersModule } from 'src/users/users.module';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({ path: path.join(__dirname, '../.env') });
 
 @Module({
-    imports: [
-        PassportModule,
-        JwtModule.register({
-            secret: 'asif123',
-            signOptions: {
-                expiresIn: '1h'
-            }
-        }),
-        UsersModule
-    ],
-    controllers: [AuthController],
-    providers: [AuthService, PassportJWTStrategy]
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'asif123',
+      signOptions: {
+        expiresIn: '1h',
+      },
+    }),
+    UsersModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, PassportJWTStrategy],
 })
-export class AuthModule { }
+export class AuthModule {}
--- frontend/src/Pages/Dashboard/Dashboard.jsx
+++ frontend/src/Pages/Dashboard/Dashboard.jsx
@@ -2,13 +2,23 @@ import React, { useState } from "react";
 import "./dashboard.css";
 import { Button } from "@mui/material";
 import Dashboard_Comp from "../../Components/Dashboard_Comp";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
   const [tabValue, setTabValue] = useState("1");
   const [value, setValue] = useState(1);
+  const navigate = useNavigate();
 
   const handleTabChange = (event, newValue) => {
     setTabValue(newValue);
+  };
+
+  const handleLogout = () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      localStorage.removeItem("token");
+      navigate("/login");
+    }
   };
 
   return (
@@ -16,7 +26,9 @@ const Dashboard = () => {
       <header className="dsb-header">
         <nav>
           <h1>weStream</h1>
-          <Button variant="outlined">My Account</Button>
+          <Button variant="outlined" onClick={handleLogout}>
+            Logout
+          </Button>
         </nav>
       </header>
       <div className="dsb-section">
@@ -36,14 +48,6 @@ const Dashboard = () => {
         ) : (
           ""
         )}
-        <div className="right-section">
-          <Button variant="text" onClick={() => setValue(1)}>
-            Home
-          </Button>
-          <Button variant="text" onClick={() => setValue(2)}>
-            Members
-          </Button>
-        </div>
       </div>
     </div>
   );
