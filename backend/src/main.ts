89e72d0f5a31aed7152dc13aa968ae98467085b3
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2024-11-28 22:01:15 +0530

    Production ready

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { createSocketConnection } from './websocket/socketConnection.js';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+import * as express from 'express';
+
+const expressServer = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressServer),
+  );
   app.useGlobalPipes(new ValidationPipe());
-  const server = await app.listen(3000);
-  createSocketConnection(server);
+  app.enableCors();
+
+  const httpServer = await app.listen(3000);
+  createSocketConnection(httpServer);
 }
+
 bootstrap();
